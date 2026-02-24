7766be9cbc6d72c718a719a9f2572ca5a41f0b6c
MD Ibna Abid <mdabout2@gmail.com>
2026-02-24 14:26:28 +0600

    add toggle btn

--- job.js
+++ job.js
@@ -15,3 +15,44 @@ let totalBtn = document.getElementById("totalBtn");
 let interviewBtn = document.getElementById("interviewBtn");
 let rejectedBtn = document.getElementById("rejectedBtn");
 
+let interviewContainer = document.createElement("div");
+let rejectedContainer = document.createElement("div");
+interviewContainer.id = "interviewContainer";
+rejectedContainer.id = "rejectedContainer";
+interviewContainer.classList.add("hidden");
+rejectedContainer.classList.add("hidden");
+filterContainer.appendChild(interviewContainer);
+filterContainer.appendChild(rejectedContainer);
+
+function count() {
+  totalValue.innerText = document.querySelectorAll(".job-card").length;
+  interviewValue.innerText = interviewCount.length;
+  rejectedValue.innerText = rejectedCount.length;
+}
+
+function toggle(id) {
+  [totalBtn, interviewBtn, rejectedBtn].forEach(btn => btn.classList.remove("bg-violet-400", "text-white"));
+  document.getElementById(id).classList.add("bg-violet-400", "text-white");
+
+  if (id === "totalBtn") {
+    cardsContainer.style.display = "block";
+    filterContainer.style.display = "none";
+  } else {
+    cardsContainer.style.display = "none";
+    filterContainer.style.display = "block";
+
+    if (id === "interviewBtn") {
+      interviewContainer.innerHTML = "";
+      interviewCount.forEach(card => interviewContainer.appendChild(card));
+      interviewContainer.classList.remove("hidden");
+      rejectedContainer.classList.add("hidden");
+      checkEmpty("interview");
+    } else if (id === "rejectedBtn") {
+      rejectedContainer.innerHTML = "";
+      rejectedCount.forEach(card => rejectedContainer.appendChild(card));
+      rejectedContainer.classList.remove("hidden");
+      interviewContainer.classList.add("hidden");
+      checkEmpty("rejected");
+    }
+  }
+}
