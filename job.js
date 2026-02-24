7c5d0507a7f89cb578074b4a3468e0c76a4c421d
MD Ibna Abid <mdabout2@gmail.com>
2026-02-24 14:21:35 +0600

    import values of text

---
+++ job.js
@@ -0,0 +1,10 @@
+let interviewCount = [];
+let rejectedCount = [];
+
+let cardsContainer = document.getElementById("allcards");
+let filterContainer = document.getElementById("filter-section");
+const emptyMessage = document.getElementById("empty-message");
+
+let totalValue = document.getElementById("totalvalue");
+let interviewValue = document.getElementById("interviewvalue");
+let rejectedValue = document.getElementById("rejectedvalue");
